91658d7ae1f7d1f68531867d3de6bdaecddb9b2d
hitcherland <hitcherland@gmail.com>
2019-06-19 20:55:00 +0100

    added options and some visual improvements

--- src/App.js
+++ src/App.js
@@ -1,96 +1,150 @@
 import React from 'react';
 import './App.css';
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) === variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+}
 
 class Cloth extends React.Component {
-    getPinholes(threadCount, border) {
-        const r = 1 - border * 2;
-        const dr = 2 * r / threadCount;
+    getPinholes(threadCount) {
+        const r = 1;
         var output = [];
-        for(var x=-r; x<=r; x+=dr) {
-            for(var y=-r; y<r; y+=dr) {
+        for(var i=0; i<=threadCount; i++) {
+            var row = [];
+            var x = ( 2 * i / threadCount - 1) * r;
+            for(var j=0; j<=threadCount; j++) {
+                var y = ( 2 * j / threadCount - 1) * r;
                 var dx = 0.002 * (Math.random() - 0.5);
                 var dy = 0.002 * (Math.random() - 0.5);
-                output.push({x: x + dx, y: y + dy});
+                row.push({x: x + dx, y: y + dy});
             }
+            output.push(row);
         }
         return output;
     }
 
     getPinholeNeighbours(pinholes) {
-        var neighbours = {}
         var l = pinholes.length; 
-        var L = Math.sqrt(l);
-        var x = null;
-        for(var i=0; i<l; i++) {
-            neighbours[i] = [];
-            if(i + 1 % L > i) {
-                neighbours[i].push(i + 1);
-                if((i + 1 + L) < l )
-                    neighbours[i].push(i + L + 1);
+        var neighbours = [];
+        for(var i=0; i<=l; i++) {
+            var neighbour_row = [];
+            for(var j=0; j<=l; j++) {
+                var neighbour = [];
+                if(i > 0) {
+                    neighbour.push([i-1, j]);
+                    /*if(j + 1 < l)
+                        neighbour.push([i-1, j+1]);
 
-                if((i + 1 - L) >= 0 )
-                    neighbours[i].push(i - L + 1);
+                    if(j - 1 > 0)
+                        neighbour.push([i-1, j-1]);
+                    */
+                }
+
+                if(i + 1 < l) {
+                    neighbour.push([i+1, j]);
+                    /*
+                    if(j + 1 < l)
+                        neighbour.push([i+1, j+1]);
+
+                    if(j - 1 > 0)
+                        neighbour.push([i+1, j-1]);
+                    */
+
+                }
+
+                if(j > 0)
+                    neighbour.push([i, j-1]);
+
+                if(j + 1 < l)
+                    neighbour.push([i, j+1]);
+                neighbour_row.push(neighbour)
             }
-
-            if((i - 1 + L) % L < i) {
-                neighbours[i].push(i - 1);
-
-                if((i - 1 + L) < l )
-                    neighbours[i].push(i + L - 1);
-
-                if((i - 1 - L) >= 0 )
-                    neighbours[i].push(i - L - 1);
-            }
-
-            if(i + L < l)
-                neighbours[i].push(i + L);
-
-            if(i - L >= 0)
-                neighbours[i].push(i - L);
-
+            neighbours.push(neighbour_row);
         }
-        return neighbours
+        return neighbours;
+    }
+
+    regenerate() {
+        const threadCount = this.state.threadCount;
+        var pinholes = this.getPinholes(threadCount);
+        var neighbours = this.getPinholeNeighbours(pinholes);
+        this.setState({
+            pinholes: pinholes,
+            pinholeNeighbours: neighbours
+        }, () => {
+            this.generatePaths();
+        });
     }
 
     constructor(props) {
         super(props)
-        var threadCount = this.props.threadCount || 200;
-        var border = this.props.border || 0.02;
-        var pinholes = this.getPinholes(threadCount, border);
+        var threadCount = getQueryVariable("threadCount") || this.props.threadCount || 400;
+        var pinholes = this.getPinholes(threadCount);
         var neighbours = this.getPinholeNeighbours(pinholes);
 
         this.state = {
             pinholes: pinholes,
             pinholeNeighbours: neighbours,
             paths: [],
-            width: this.props.width || 64,
-            height: this.props.height || 64,
+            width: 64,
+            height: 64,
             resizeListener: undefined,
             threadCount: threadCount,
-            borderSize: border,
-            numThreads: this.props.numThreads || 5,
-            numCurves: this.props.numCurves || 5
+            numThreads: getQueryVariable("nthreads") || this.props.nthreads || 5,
+            numCurves: getQueryVariable("ncurves") || this.props.ncurves || 5,
+            speed: getQueryVariable("speed") || this.props.speed || 64,
+            frame: 0,
+            then: performance.now()
         }
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-        var needleIndex = Math.floor(Math.floor(Math.random() * this.state.pinholes.length))
+    handleClick() {
+        this.generatePaths();
+    }
+
+    getRandomPinhole() {
+        var goalX = Math.floor(Math.random() * this.state.pinholes.length);
+        var goalY = Math.floor(Math.random() * this.state.pinholes[goalX].length);
+        return [goalX, goalY];
+    }
+
+    generatePaths() {
+        var paths = [];
         for(var thread_index=0; thread_index<this.state.numThreads; thread_index++) {
+            var needleIndex = this.getRandomPinhole();
             var path = [needleIndex];
             for(var thread_goal_iter=0; thread_goal_iter<this.state.numCurves; thread_goal_iter++) {
-                var goalIndex = Math.floor(Math.random() * this.state.pinholes.length);
+                var goalIndex = this.getRandomPinhole();
                 var goalPath = this.getPathBetweenPoints(needleIndex, goalIndex);
                 path = path.concat(goalPath);
                 needleIndex = goalIndex;
             }
-            this.state.paths.push({
+            goalIndex = path[0];
+            goalPath = this.getPathBetweenPoints(needleIndex, goalIndex);
+            path = path.concat(goalPath);
+
+            paths.push({
                 path: path,
-                color: this.getRandomColor()
+                color: this.getRandomColor(),
+                show: Math.random()
             });
         }
+
+        this.setState({
+            paths: paths
+        }, () => this.draw());
     }
 
     getNextPinhole(i, goal) {
-        var min = this.state.pinholeNeighbours[i].reduce((o, v) => {
-            var ph = this.state.pinholes[v];
+        var min = this.state.pinholeNeighbours[i[0]][i[1]].reduce((o, v) => {
+            var ph = this.state.pinholes[v[0]][v[1]];
             var dist_to_goal = Math.sqrt(Math.pow(ph.x - goal.x, 2) + Math.pow(ph.y - goal.y, 2));
             if(dist_to_goal < o[0])
                 return [dist_to_goal, v];
@@ -102,12 +156,12 @@ class Cloth extends React.Component {
 
     getPathBetweenPoints(index_a, index_b) {
         var output = [index_a];
-        var goal = this.state.pinholes[index_b];
+        var goal = this.state.pinholes[index_b[0]][index_b[1]];
         var next = index_a;
 
         while(true) {
             next = this.getNextPinhole(next, goal); 
-            if(next === index_b)
+            if(next[0] === index_b[0] && next[1] === index_b[1])
                 break;
             output.push(next);
         }
@@ -126,11 +180,22 @@ class Cloth extends React.Component {
     }
 
     resize() {
+        var el = this.refs.canvas;
         var par = this.refs.canvas.parentElement;
-        console.log(par.clientWidth, par.clientHeight);
+        var elStyle = window.getComputedStyle(el, null);
+        var style = window.getComputedStyle(par, null);
+        var height = par.clientHeight;  // height with padding
+        var width = par.clientWidth;   // width with padding
+
+        height -= parseFloat(style.paddingTop) + parseFloat(style.paddingBottom);
+        width -= parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
+        height -= parseFloat(elStyle.borderTopWidth) + parseFloat(elStyle.borderBottomWidth);
+        width -= parseFloat(elStyle.borderLeftWidth) + parseFloat(elStyle.borderRightWidth);
+
+        var min = Math.min(width, height);
         this.setState({
-            width: par.clientWidth,
-            height: par.clientHeight
+            width: min,
+            height: min, 
         }, () => this.draw())
     }
 
@@ -139,38 +204,55 @@ class Cloth extends React.Component {
         const ctx = canvas.getContext("2d");
         const midX = this.state.width / 2;
         const midY = this.state.height / 2;
-        const radius = Math.min(midX, midY) * 0.9;
-        const innerRadius = (1.0 - this.state.borderSize * 2) * radius;
+        const radius = Math.min(midX, midY);
+        const threadCount = this.state.threadCount;
 
         ctx.fillStyle = '#c19a6b';
+        ctx.fillStyle = '#F1F1D4';
         ctx.beginPath();
         ctx.rect(midX - radius, midY - radius, 2 * radius, 2 * radius);
         ctx.fill();
 
-        ctx.fillStyle = '#F1F1D4';
-        ctx.beginPath();
-        ctx.rect(midX - innerRadius, midY - innerRadius, 2 * innerRadius, 2 * innerRadius);
-        ctx.fill();
-
+        ctx.fillStyle = 'rgba(255,255,255,0.5)';
         for(var i=0; i<this.state.pinholes.length; i++) {
-            var pinhole = this.state.pinholes[i];
-            ctx.fillStyle = '#FFF';
-            var x = midX + radius * pinhole.x;
-            var y = midY + radius * pinhole.y;
-            ctx.beginPath();
-            ctx.arc(x, y, radius / 250, 0, 2 * Math.PI);
-            ctx.fill();
+            var row = this.state.pinholes[i];
+            for(var pinhole of row) {
+                var x = midX + radius * pinhole.x;
+                var y = midY + radius * pinhole.y;
+                ctx.beginPath();
+                ctx.arc(x, y, radius / threadCount, 0, 2 * Math.PI);
+                ctx.fill();
+            }
         }
+        this.renderThreads();
+    }
 
-        ctx.lineWidth = radius / 150;
+    renderThreads() {
+        const canvas = this.refs.canvas;
+        const ctx = canvas.getContext("2d");
+        const midX = this.state.width / 2;
+        const midY = this.state.height / 2;
+        const radius = Math.min(midX, midY);
+        const threadCount = this.state.threadCount;
+        const frame = this.state.frame;
+
+        ctx.lineWidth = radius * 1.5 / threadCount;
         for(var path of this.state.paths) {
             ctx.strokeStyle = path.color;
             
             ctx.beginPath();
             var begin=true
+            var l = path.path.length;
+            var L = path.show * l;
+            var start = (frame % l);
+            var stop = start + L;
 
-            for(var pathIndex of path.path) {
-                var point = this.state.pinholes[pathIndex];
+            var subpaths = path.path.slice(start, stop);
+            if(stop > l) {
+                subpaths = subpaths.concat(path.path.slice(0, stop - l))
+            }
+            for(var pathIndex of subpaths) {
+                var point = this.state.pinholes[pathIndex[0]][pathIndex[1]];
                 var x = midX + radius * point.x;
                 var y = midY + radius * point.y;
                 if(begin) {
@@ -178,6 +260,7 @@ class Cloth extends React.Component {
                     begin=false;
                 } else {
                     ctx.lineTo(x, y);
+                    begin=true;
                 }
             }
             ctx.stroke();
@@ -185,28 +268,124 @@ class Cloth extends React.Component {
     }
 
     componentDidMount() {
-        this.resize();
         this.setState({
             resizeListener: window.addEventListener("resize", () => this.resize())
         });
+        this.generatePaths();
+        this.resize();
+        this.tick();
     }
 
     componentWillUnmount() {
         window.removeEventListener("resize", this.state.resizeListener);
+        cancelAnimationFrame(this.animFunc);
     };
 
+    tick() {
+        this.animFunc = requestAnimationFrame(() => this.tick());
+        var now = performance.now()
+        const then = this.state.then;
+        const speed = this.state.speed;
+        if(now - then > speed) {
+            this.setState((state, props) => ({
+                frame: state.frame + 1,
+                then: now 
+            }), this.draw);
+        }
+    }
+
     render() {
-        var canvas = <canvas ref="canvas" width={this.state.width} height={this.state.height}></canvas>;
+        var style = {
+            border: "12px solid #c19a6b",
+            boxSizing:"border-box",
+            borderRadius: "12px",
+        }
+        var canvas = <canvas ref="canvas" onClick={this.handleClick} width={this.state.width} height={this.state.height} style={style}></canvas>;
         return canvas;
     }
 }
 
-function App() {
-  return (
-      <div className="flower">
-        <Cloth numThreads="50"/>
-      </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            nthreads: 8,
+            ncurves: 3,
+            threadCount: 150,
+            speed: 64,
+        }
+        this.handleClick = this.handleClick.bind(this);
+        this.change_nthreads = this.change_nthreads.bind(this);
+        this.change_ncurves = this.change_ncurves.bind(this);
+        this.change_threadCount = this.change_threadCount.bind(this);
+        this.change_speed = this.change_speed.bind(this);
+    }
+
+    handleClick() {
+        this.refs.cloth.handleClick();
+    }
+
+    change_nthreads(event) {
+        var value = event.target.value;
+        this.setState({nthreads: value}, () => {
+            this.refs.cloth.setState({numThreads: value}, () => {
+                this.refs.cloth.generatePaths()
+            });
+        });
+    }
+
+    change_ncurves(event) {
+        var value = event.target.value;
+        this.setState({ncurves: value}, () => {
+            this.refs.cloth.setState({numCurves: value}, () => {
+                this.refs.cloth.generatePaths()
+            });
+        });
+    }
+
+    change_threadCount(event) {
+        this.setState({threadCount: event.target.value});
+        this.refs.cloth.setState({threadCount: event.target.value}, () =>
+            this.refs.cloth.regenerate()
+        );
+    }
+
+
+    change_speed(event) {
+        this.setState({speed: event.target.value});
+        this.refs.cloth.setState({speed: event.target.value}, () =>
+            this.refs.cloth.regenerate()
+        );
+    }
+
+    render() {
+        return (
+            <div className="full">
+                <Cloth ref="cloth" nthreads={this.state.nthreads} ncurves={this.state.ncurves} threadCount={this.state.threadCount} speed={this.state.speed}/>
+                <div style={{float: "left"}} className="card">
+                    <div className="form-group">
+                        <label>numerosity</label>
+                        <input ref="nthreads" type="range" className="form-control-range" min="1" max="20" value={this.state.nthreads} onChange={this.change_nthreads} />
+                    </div>
+                    <div className="form-group">
+                        <label>travelness</label>
+                        <input ref="ncurves" type="range" className="form-control-range" min="2" max="10" value={this.state.ncurves} onChange={this.change_ncurves} />
+                    </div>
+                    <div className="form-group">
+                        <label>luxurity</label>
+                        <input ref="threadCount" type="range" className="form-control-range" min="1" max="250" value={this.state.threadCount} onChange={this.change_threadCount} />
+                    </div>
+                    <div className="form-group">
+                        <label>relaxedness</label>
+                        <input ref="speed" type="range" className="form-control-range" min="16" max="1000" value={this.state.speed} onChange={this.change_speed} />
+                    </div>
+ 
+                    <button onClick={this.handleClick} className="btn btn-primary" type="button">new threads</button>
+                </div>
+
+            </div>
+          );
+    }
 }
 
 export default App;
